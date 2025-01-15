3eca0b6d32918e289e43918f02c51e45241d00e3
Promastalex <sasakocetov7197@gmail.com>
2025-01-15 10:59:12 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Александр"
+print("Имя пользователя:", username)
+title = "Проект"
+print("Заголовок заметки:", title)
+content = "Сделать проект"
+print("Описание заметки:", content)
+status = "Активная"
+print("Статус заметки:", status)
+created_date = "14-01-2025"
+print("Дата создания заметки:", created_date)
+issue_data = "14-11-2025"
+print("Дата истечения заметки:", issue_data)
